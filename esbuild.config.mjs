689c85d23388edcf7ca37090605e6bfb81fa376b
Joe Butler <youremail@example.com>
2026-01-07 16:57:02 +0000

    chore(build): output directly to main.js to fix hot-reload

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -26,7 +26,7 @@ const ctx = await esbuild.context({
     "@codemirror/view",
     "@lezer/common"
   ],
-  outfile: "dist/main.js",
+  outfile: "main.js",
   sourcemap: isProd ? false : "inline",
   plugins: [patchJkanban],
   loader: {
